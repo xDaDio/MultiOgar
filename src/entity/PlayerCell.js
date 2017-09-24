f484fa0dd48e3c69c47f3bfb0dfd040e78c7cbcd
ＲＡＩＮＢＯＩ <Abdessalam.sgh.2001@gmail.com>
2017-09-24 18:57:41 +0100

    Add files via upload

---
+++ src/entity/PlayerCell.js
@@ -0,0 +1,36 @@
+var Cell = require('./Cell');
+
+function PlayerCell() {
+    Cell.apply(this, Array.prototype.slice.call(arguments));
+    this.cellType = 0;
+    this._canRemerge = false;
+}
+
+module.exports = PlayerCell;
+PlayerCell.prototype = new Cell();
+
+// Main Functions
+
+PlayerCell.prototype.canEat = function (cell) {
+    return true; // player cell can eat anyone
+};
+
+PlayerCell.prototype.getSpeed = function (dist) {
+    var speed = 2.2 * Math.pow(this._size, -0.439);
+    speed *= 40 * this.gameServer.config.playerSpeed;
+    return Math.min(dist, speed) / dist;
+};
+
+PlayerCell.prototype.onAdd = function (gameServer) {
+    // Add to player nodes list
+    this.gameServer.nodesPlayer.push(this);
+};
+
+PlayerCell.prototype.onRemove = function (gameServer) {
+    // Remove from player cell list
+    var index = this.owner.cells.indexOf(this);
+    if (index != -1) this.owner.cells.splice(index, 1);
+
+    index = this.gameServer.nodesPlayer.indexOf(this);
+    if (index != -1) this.gameServer.nodesPlayer.splice(index, 1);
+};
